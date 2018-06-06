3e67d9993338a6e02c0bdce949a06be7a10584a8
rizkyario <rizkyario@yahoo.com>
2018-06-06 20:05:31 +0200

    Utilize functional programming

--- at_parser.scala
+++ at_parser.scala
@@ -1,42 +1,26 @@
 import scala.util.matching.Regex
 import scala.io.Source
-import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.{LinkedHashMap => Map}
 import scala.util.control.Breaks._
 
 object at_parser {
-	def buildRules(rules: Map[String, String]): Regex = 
-	{
-		var rule = ""
-		var i = 0
-	
-		for ((k, v) <- rules)
-		{
-			if (i > 0) rule += " "
-			rule += v
-			i = i + 1
-		}
-		(s"$rule".r)
-	}
-
 	def parseValue(line: String, rules: Map[String, String]): Map[String, String] = 
 	{
-		var keys = Seq[String]()
 		var values = Map[String, String]()
 		var i = 0
-		var re = buildRules(rules)
+		val re = s"${rules.values.reduce((a, b) => a + " " + b)}".r
 
-		for (key <- rules.keys) keys = keys:+ key
 		for (m <- re.findAllIn(line).matchData; e <- m.subgroups) 
 		{
-			values += ((keys(i) -> e))
+			values += ((rules.keys.toSeq(i) -> e))
 			i += 1
 		}
 		println(values)
 		values
 	}
 	
-   	def main(args: Array[String]) {
+   	def main(args: Array[String])
+	{
 	   	val filename = args(0)
 		val combineRules = Map(
 			("ip", "(\\S+)"),
@@ -48,7 +32,8 @@ object at_parser {
 			("bytes", "(\\S+)"),
 			("referer", "\"(.*?)\""),
 			("agent", "\"(.*?)\""),
-		)	
+		)
+
 		for (line <- Source.fromFile(filename).getLines) {
 			println(parseValue(line, combineRules))
 		}
