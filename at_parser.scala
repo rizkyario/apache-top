48b887b89783975ef67ecc3bd0ba3954e04d1246
rizkyario <rizkyario@yahoo.com>
2018-06-06 23:15:32 +0200

    Minimize var

--- at_parser.scala
+++ at_parser.scala
@@ -1,28 +1,25 @@
 import scala.util.matching.Regex
 import scala.io.Source
-import scala.collection.mutable.{LinkedHashMap => Map}
+import scala.collection.mutable.LinkedHashMap
 import scala.util.control.Breaks._
 
 object at_parser {
-	def parseValue(line: String, rules: Map[String, String]): Map[String, String] = 
+	def parseValue(line: String, rules: LinkedHashMap[String, String]): Map[String, String] = 
 	{
-		var values = Map[String, String]()
-		var i = 0
 		val re = s"${rules.values.reduce((a, b) => a + " " + b)}".r
-
-		for (m <- re.findAllIn(line).matchData; e <- m.subgroups) 
-		{
-			values += ((rules.keys.toSeq(i) -> e))
-			i += 1
+		val values = for {
+			m <- re.findAllIn(line).matchData
+			(e, i) <- m.subgroups.zipWithIndex
+		} yield {
+			rules.keys.toSeq(i) -> e
 		}
-		println(values)
-		values
+		values.map(c => c._1 -> c._2).toMap
 	}
 	
    	def main(args: Array[String])
 	{
 	   	val filename = args(0)
-		val combineRules = Map(
+		val combineRules = LinkedHashMap(
 			("ip", "(\\S+)"),
 			("client", "(\\S+)"),
 			("user", "(\\S+)"),
@@ -33,7 +30,6 @@ object at_parser {
 			("referer", "\"(.*?)\""),
 			("agent", "\"(.*?)\""),
 		)
-
 		for (line <- Source.fromFile(filename).getLines) {
 			println(parseValue(line, combineRules))
 		}
