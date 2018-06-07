6f0630f34a15352be9bc030e03550b87e00b136b
Rizky Ario <rizkyario@yahoo.com>
2018-06-07 20:51:19 +0200

    Setup SBT
    #10 #14

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "apache-top"
+
+version := "1.0"
+
+scalaVersion := "2.12.6"
+
+scalacOptions += "-deprecation"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ src/main/scala/apache_top.scala
@@ -0,0 +1,40 @@
+package com.rizkyario.apache_top
+
+import scala.io.Source
+import java.io.File
+
+object apache_top
+{	
+   	def main(args: Array[String])
+	{
+		if (args.length == 0)
+		{
+			println("Usage ./apache_top [log_file]")
+			sys.exit(0)
+		}
+	   	val filename = args(0)
+		val delay = 1000
+		val parser = new ApacheTopParser
+		val printer = new ApacheTopPrinter(filename)
+		var fileSize: Long = 0
+		while (true)
+		{
+			val nfileSize = new File(filename).length
+			if (nfileSize != fileSize)
+			{
+				fileSize = nfileSize
+				ApacheTopPrinter clearScreen
+				val logs = (for (line <- Source.fromFile(filename).getLines if line.length != 0)
+				yield
+				{
+					val log = parser.parseLog(line)
+					log
+				}).toList
+				printer.printSummaryLog(logs)
+				printer.printVisitorLog(logs, 20)
+				printer.printRequestLog(logs, 20)
+			}
+			Thread.sleep(delay)
+		}
+	}
+}
