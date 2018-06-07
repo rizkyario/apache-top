ddfc08802d944bd36a87fd062b60674b51466e8d
rizkyario <rizkyario@yahoo.com>
2018-06-07 03:06:41 +0200

    Add displayVisitorLog

--- apache_top.scala
+++ apache_top.scala
@@ -18,14 +18,32 @@ object apache_top {
 		}) toMap
 	}
 
-	def displayRequestedLog(logs: mutable.MutableList[Map[String, String]]) =
+	def displayVisitorLog(logs: mutable.MutableList[Map[String, String]]) =
 	{
-		val uUrls = for
+		val days = (for
+		{
+			(key, xs) <- logs.groupBy(_.get("timestamp"))
+		} yield (key, xs.length)).toSeq.sortWith(_._2 > _._2)
+
+		for ((url, i) <- days.zipWithIndex)
+		{
+			if (i < 10)
+				println(f"${url._2}: ${url._1}")
+		}
+	}
+
+	def displayRequestLog(logs: mutable.MutableList[Map[String, String]]) =
+	{
+		val uUrls = (for
 		{
 			(key, xs) <- logs.groupBy(_.get("request"))
-		} yield (key, xs.length)
+		} yield (key, xs.length)).toSeq.sortWith(_._2 > _._2)
 
-		println(uUrls)
+		for ((url, i) <- uUrls.zipWithIndex)
+		{
+			if (i < 10)
+				println(f"${url._2}: ${url._1}")
+		}
 	}
 
 	def displayLog(filename: String, logs: mutable.MutableList[Map[String, String]]) =
@@ -33,27 +51,26 @@ object apache_top {
 		print("\033[H\033[J")
 		val fileSize = new File(filename).length
 		val size = logs.foldLeft(0){(total, log)=>{total + log("bytes").toInt}}
-		val request = logs.length
-		val fRequest = logs.filter((log)=>(log("status").toInt >= 400)).size
-		val vRequest = logs.filter((log)=>(log("status").toInt < 400)).size
-
+		val failedRequests = logs.filter((log)=>(log("status").toInt >= 400)).size
+		val validRequests = logs.filter((log)=>(log("status").toInt < 400)).size
 		val visitors = (for (log <- logs) yield (log("ip")))
 		val referrers = (for (log <- logs) yield (log("referrer")))
 		val urls = (for (log <- logs) yield (log("request").split(" ")(1)))
 		val req404s = for (log <- logs; if log("status").toInt == 404 ) yield {log("status") -> log("request")}
+
 		println("\n** APACHE TOP Overall Analysed Requests **\n")
 
-		println(f"Total Request   ${request }%-6d  Unique Visitors  ${visitors.distinct.length}%-6d        Referrers   ${referrers.distinct.length}%-6d  Log Source  $filename")
-		println(f"Valid Request   ${vRequest}%-6d  Unique Files     ${urls.distinct.length    }%-6d        Unique 404  ${req404s.distinct.length  }%-6d  Log Size    ${fileSize.toFloat/1000    }%-4.2f KiB")
-		println(f"Failed Request  ${fRequest}%-6d  Bandwidth        ${size.toFloat/1000       }%-4.2f KiB")
+		println(f"Total Request   ${logs.length   }%-6d  Unique Visitors  ${visitors.distinct.length}%-6d        Referrers   ${referrers.distinct.length}%-6d  Log Source  $filename")
+		println(f"Valid Request   ${validRequests }%-6d  Unique Files     ${urls.distinct.length    }%-6d        Unique 404  ${req404s.distinct.length  }%-6d  Log Size    ${fileSize.toFloat/1000    }%-4.2f KiB")
+		println(f"Failed Request  ${failedRequests}%-6d  Bandwidth        ${size.toFloat/1000       }%-4.2f KiB")
 
 		println("\n** 1. Unique Visitor per Day **\n")
 		
-		println("WIP")
+		displayVisitorLog(logs)
 
-		println("\n** 2. Requested Files (URLs) **\n")
+		println("\n** 2. Top 10 Requested Files (URLs) **\n")
 
-		displayRequestedLog(logs)
+		displayRequestLog(logs)
 	}
 	
    	def main(args: Array[String])
